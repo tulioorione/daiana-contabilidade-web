0b38ceecd7e9d52ffee72e375829edc961d469de
Tulio Orione <tulioorione@gmail.com>
2026-02-05 17:49:18 -0300

    Atualiza grade de serviços com conteúdo oficial da Daiana

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.querySelector('.contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nome = this.querySelector('input[type="text"]').value;
+    const email = this.querySelector('input[type="email"]').value;
+    const mensagem = this.querySelector('textarea').value;
+
+    if (nome && email && mensagem) {
+        alert(`Obrigado ${nome}! Sua mensagem foi enviada com sucesso.`);
+        this.reset();
+    } else {
+        alert('Por favor, preencha todos os campos corretamente.');
+    }
+});
+
+console.log("Script da Daiana Contabilidade carregado com sucesso!");
