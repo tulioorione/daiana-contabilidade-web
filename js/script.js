61f89121e1dbf4e4173ebf08bb9ae24c0a16a532
Tulio Orione <tulioorione@gmail.com>
2026-02-05 17:53:08 -0300

    implementa responsividade e menu hamburguer para dispositivos móveis

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ document.querySelector('.contact-form').addEventListener('submit', function(even
 });
 
 console.log("Script da Daiana Contabilidade carregado com sucesso!");
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    
+    menuToggle.classList.toggle('is-active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
+
